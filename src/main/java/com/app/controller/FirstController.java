d7fd4e11cd3e3b3319b0ded635bf92f5cfe5c018
Rahul <'rk7373914@gmail.com'>
2023-07-25 19:53:22 +0530

    This is first commit of demo project.

---
+++ src/main/java/com/app/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class FirstController {
+	
+	@GetMapping("/home")
+	public String getHomePage() {
+		return "index";
+	}
+}
